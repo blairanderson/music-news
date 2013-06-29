ffac56777ca25e2b13007fab738584aa83e0407f
Blair Anderson <blair81@gmail.com>
2013-06-28 19:44:46 -0600

    WIP bandcamp embed

--- app/models/bandcamp.rb
+++ app/models/bandcamp.rb
@@ -1,3 +1,13 @@
 class Bandcamp < Song
   include Attachment
+
+  def parse_for_embed
+    
+  end
+
+  def get_band_and_track_id
+    #hit this url with httparty
+    # pass the results into the embed url
+    "http://api.bandcamp.com/api/url/1/info?key=vatnajokull&url=#{url}"
+  end
 end
