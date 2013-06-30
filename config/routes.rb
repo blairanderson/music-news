dd0a72f7edbde540ceb5a40071d2f5e5ce27f505
Blair Anderson <blair81@gmail.com>
2013-06-29 22:32:11 -0600

    adding /new route for new submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MusicNews::Application.routes.draw do
   resources :submissions
   root 'submissions#index'
+  get '/new', to: 'submissions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
