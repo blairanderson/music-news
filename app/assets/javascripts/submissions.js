3d87f9598e8ec6ab5b8b594e3c921f142641e7de
Blair Anderson <blair81@gmail.com>
2013-07-07 18:48:29 -0600

    add assets, precompiled, and some minor css styling

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -1,11 +1,18 @@
-$.ajaxSetup ({ cache: true }); 
+$.ajaxSetup ({ cache: true });
 $(document).ready(function(){
   $(".soundcloud").each(function( index ){
     var element = this;
     var target = $(this).data('soundcloud');
     $.ajax({
       url: "http://soundcloud.com/oembed.json",
-      data: {url: target, color: "000000"},
+      data: {
+        url: target,
+        color: "000000",
+        theme_color: "000000",
+        buying: "false",
+        sharing: "false",
+        download: "false",
+        show_bpm: 'false'},
       success: function (response) {
         var iframe = response.html;
         $(element).html(response.html);
