20487991c8c1e30129e653c6075800989705f38a
blair anderson <blair81@gmail.com>
2013-12-21 13:55:05 -0700

    song and submission models get a facelift

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,10 @@
 class Song < ActiveRecord::Base
-  include Attachment
   self.inheritance_column = :_type_disabled
 
-  belongs_to :submission
+  belongs_to :submission, counter_cache: true
   scope :soundclouds, -> { where(type: "Soundcloud") }
-  scope :bandcamps, -> { where(type: "Bandcamp") }
 
-  validates :url, :format => { :with => /\A(?:https?:\/\/soundcloud\.com)\/.*/ }
+  validates :url, presence: true
 
   def has_details?
      stream_url? && embed?
@@ -14,28 +12,6 @@ class Song < ActiveRecord::Base
 
   def need_details?
     !has_details?
-  end
-
-  def fetch_details
-    resolve && get_stats
-  end
-
-  def get_stats
-    client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'])
-    begin
-      track = client.get('/resolve', :url => url)
-      
-      update(stream_url: track.stream_url)
-      update(comment_count: track.comment_count)
-      update(download_count: track.download_count)
-      update(playback_count: track.playback_count)
-      update(favoritings_count:  track.favoritings_count)
-      update(active: "true")
-
-      track
-    rescue
-      update(active: "false")
-    end    
   end
 
   def resolve
@@ -48,10 +24,16 @@ class Song < ActiveRecord::Base
       update(active: "true")
       update(thumbnail_url: track.thumbnail_url)
 
+      track = client.get('/resolve', :url => url)
+      update(stream_url: track.stream_url)
+      update(comment_count: track.comment_count)
+      update(download_count: track.download_count)
+      update(playback_count: track.playback_count)
+      update(favoritings_count:  track.favoritings_count)
+
       track
     rescue
       update(active: "false")
     end
   end
-
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,11 +4,15 @@ class Submission < ActiveRecord::Base
   validates :body, presence: true
   validates :twitter, presence: true
   validates :email, presence: true
+  has_many :songs
+  accepts_nested_attributes_for :songs, reject_if: :missing_url?
+
+  def missing_url?(attributes)
+    !(/\A(?:https?:\/\/soundcloud\.com)\/.*/ === attributes['url'])
+  end
+  
   scope :latest, -> { order("created_at DESC") }
 
-  def songs
-    Song.where(submission_id: self.id)
-  end
 
   def next
     Submission.where(id: id + 1).first
@@ -43,10 +47,20 @@ class Submission < ActiveRecord::Base
   end
 
   def self.purge_count
-    Submission.all.each_with_index {|s,i|  (puts "#{i} - #{s.id}") if s.songs.count < 1 }
+    subs = Submission.where(songs_count: 0).count
+    sub_total = Submission.count
+    "#{subs} of #{sub_total} without songs."
   end
   
+
+  def self.update_all_song_counts
+    Submission.all.each do |s|
+      s.update songs_count: s.songs.length
+    end
+  end
+
+private
   def self.purge
-    Submission.all.each {|s| s.destroy if s.songs.count < 1}
+    Submission.where(songs_count: 0).destroy_all
   end
 end
