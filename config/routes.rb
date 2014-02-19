43bb69a288f8e716159caaa789031f7d37e52164
blair anderson <blair81@gmail.com>
2014-02-18 21:50:49 -0700

    made the routes pretty

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 MusicNews::Application.routes.draw do
   root 'submissions#backbone'
-  get '/seainhd' => redirect('http://www.seainhd.com')
-  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
 
   resources :submissions, except: [:edit, :update] do
     resource :publisher, only: [:show, :new, :create]
@@ -14,12 +12,14 @@ MusicNews::Application.routes.draw do
   end
 
   resources :songs, only: [:index, :show, :destroy]
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
 
   #vanity-URLS
-  get 'new' => 'submissions#new'
-  get 'feed' => 'submissions#feed'
-  get 'logout' => 'sessions#destroy'
-  get 'signout' => 'sessions#destroy'
+  get 'new'       => 'submissions#new'
+  get 'feed'      => 'submissions#feed'
+  get 'logout'    => 'sessions#destroy'
+  get 'signout'   => 'sessions#destroy'
+  get '/seainhd'  => redirect('http://www.seainhd.com')
 
   # backbone redirects
   get ':id' => 'submissions#backbone_redirect'
