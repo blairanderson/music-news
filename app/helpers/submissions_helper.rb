612d0e781ca8554ff82fe528a9a419e5fb00a381
Blair Anderson <Blair@quickleft.com>
2014-03-25 20:05:39 -0700

    removing dead comments

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,18 +1,2 @@
 module SubmissionsHelper
-
-  def soundcloud_link(soundcloud_url)
-    link_to cleanup(soundcloud_url), soundcloud_url, target: "_blank"
-  end
-
-  def cleanup(url)
-    url.gsub('https://','').gsub('http://', '').gsub('soundcloud.com/','')
-  end
-
-  def clean_title(title)
-    if title.nil?
-      "learn more..."
-    else
-      title[0..25]
-    end
-  end
 end
---
+++ spec/helpers/submissions_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe SubmissionsHelper do
+end
