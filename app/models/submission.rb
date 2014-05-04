228fc97467d1644a3552032dcfcc1554dcf64d6d
blair anderson <blair81@gmail.com>
2014-05-04 12:19:55 -0600

    removing dead code

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -18,43 +18,15 @@ class Submission < ActiveRecord::Base
   end
 
   scope :latest, -> { order("created_at DESC") }
+  scope :soundclouds, -> { where(type: :Soundcloud) }
+  scope :bandcamps, -> { where(type: :Bandcamp) }
 
   after_save :resolve_songs
   def resolve_songs
-    songs.each do |s|
-      s.resolve
-    end
-  end
-
-  def next
-    Submission.where(id: id + 1).first
-  end
-
-  def previous
-    Submission.where(id: id - 1).first
+    songs.each {|s| s.resolve}
   end
 
   def to_param
     "#{id}-#{title[0,20]}".parameterize
-  end
-
-  def videos
-    Video.where(submission_id: self.id)
-  end
-
-  def attachments
-    songs.concat(videos)
-  end
-
-  def soundclouds
-    songs.where(:type == "Soundcloud".to_sym)
-  end
-
-  def youtubes
-    videos.where(:type == "Youtube".to_sym)
-  end
-
-  def bandcamps
-    songs.where(:type == "Bandcamp".to_sym)
   end
 end
